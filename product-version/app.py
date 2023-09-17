9a417c2835af03ee7c2e480aee76a0d7c713bcf7
JY <90738099+JY9150@users.noreply.github.com>
2023-09-17 11:15:46 +0800

    add google cloud

--- product-version/app.py
+++ product-version/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 
+# from google.cloud import aiplatform_v1
+# import vertexai
+# from vertexai.language_models import TextGenerationModel
 
 app = Flask(__name__)
 
@@ -35,9 +38,40 @@ def process_command(data):
 
 
 def LLM_handling():
-    return 0
+    vertexai.init(project="robust-team-393105", location="us-central1")
+    parameters = {
+        "candidate_count": 1,
+        "max_output_tokens": 256,
+        "temperature": 0.2,
+        "top_p": 0.8,
+        "top_k": 40
+    }
+    model = TextGenerationModel.from_pretrained("text-bison@001")
+    response = model.predict(
+        """請根據指令句A，修改指令句B的句子，產生句子C。例子如下:
+
+    A: [刪除]你好嗎?
+    B: 你好嗎? 我很好
+    C: 我很好
+
+    A: 在文章後面[新增]我很好
+    B: 一段乍看之下像是文章，但仔細一瞧全無道理的文字組合。
+    C: 一段乍看之下像是文章我很好，但仔細一瞧全無道理的文字組合。
+
+    A: 在文字錄入比賽（打字比賽）中，最公平的比賽用文本就是隨機文本
+    B: 把公平[替換成]老公公
+    C: 在文字錄入比賽（打字比賽）中，最老公公的比賽用文本就是隨機文本
+
+    則根據此A、B句，對應的句子C應為何?
+    A: 嘉義新港地震 氣象局研判因板塊擠壓無關梅山斷層 未來2週防規模4以上餘震
+    B: [刪除]氣象局
+    C:""",
+        **parameters
+    )
+    print(f"Response from Model: {response.text}")
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    LLM_handling()
