c7ad5ac1b970d8ec3160b2cd07585e656c642964
jalee <tommy920125@gmail.com>
2023-09-27 01:55:39 +0800

    commit

--- translanguaging-IME/change_dataset.py
+++ translanguaging-IME/change_dataset.py
@@ -1,40 +1,43 @@
 import random
 import string
-from CangjieKeyMap import CangjieKeyMap
 
-with open("VtoW\\translanguaging-IME\\datasets\\englishwords.txt", "r") as file:
+a = 1
+with open("translanguaging-IME\datasets\englishwords.txt", "r") as file:
     word_list = [line.strip() for line in file]
 
 with open(
-    "VtoW\\translanguaging-IME\\datasets\\chinese_news_cangjie_mix.txt",
+    "translanguaging-IME\datasets\chinese_news_cangjie_mix.txt",
     "r",
     encoding="utf-8",
 ) as input_file, open(
-    "VtoW\\translanguaging-IME\\datasets\\most15_newdataset.txt",
+    "translanguaging-IME\datasets\most15_newdataset.txt",
     "w",  # Use "a" for append mode
 ) as output_file:
     for line in input_file:
-        line = line.strip()  # Remove trailing newline and spaces
-        parts = line.split("\t")  # Split the line into English and Chinese parts
+        line = line.strip()
+        parts = line.split("\t")
         english_text = parts[0]
         chinese_text = parts[1]
-        chinese_text = " ".join(chinese_text)
+        chinese_text = "".join(chinese_text)
+        chinese_text_list = list(chinese_text)
+        english_text_list = english_text.split()
 
-        selected_word = random.choice(word_list)
+        # if a != 10 and a < 10:
+        #     print(chinese_text_list[1], english_text_list[1])
+        # a += 1
         if len(chinese_text) <= 15:
+            selected_word = random.choice(word_list)
             random_int = random.randint(0, 14)
-            chinese_text_list = list(chinese_text)
             r = " " + selected_word + " "
             chinese_text_list.insert(random_int, r)
+            english_text_list.insert(random_int, selected_word)
         if len(chinese_text) > 15:
+            selected_word = random.choice(word_list)
             random_int = random.randint(0, len(chinese_text) - 1)
-            chinese_text_list = list(chinese_text)
             r = " " + selected_word + " "
             chinese_text_list.insert(random_int, r)
-        # english_text_list = english_text.split()
-        # # print(english_text_list)
-        # english_text_list.insert(random_int, selected_word)
-        # english_text = " ".join(english_text_list)
-        # # print(random_int)
+            english_text_list.insert(random_int, selected_word)
+
+        english_text = " ".join(english_text_list)
         chinese_text = "".join(chinese_text_list)
-        output_file.write(chinese_text + "\n")
+        output_file.write(english_text + "\t" + chinese_text + "\n")
