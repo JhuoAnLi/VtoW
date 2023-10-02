e0a878647403bd62f849f8b42a39786e56cf3871
jalee <tommy920125@gmail.com>
2023-10-02 19:56:39 +0800

    commit

--- IME extension/background.js
+++ IME extension/background.js
@@ -1,11 +1,10 @@
-// This is a background script for your Chrome extension
-// You can add background-specific logic here if needed
-
-// Example: Listening for a message from a content script
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    console.log("Message received:", message);
+
     if (message.action === "convertToUppercase") {
-        // You can add background logic here if needed
-        // For now, let's just log the message
-        console.log("Received a message from content script:", message.text);
+        const convertedText = message.text.toUpperCase();
+        sendResponse({ result: convertedText });
+
+        console.log("Received a message from content script or popup:", message.text);
     }
 });
--- IME extension/popup.js
+++ IME extension/popup.js
@@ -1,15 +1,14 @@
-// Get references to the HTML elements
-const inputText = document.getElementById("inputText");
-const convertButton = document.getElementById("convertButton");
-const outputText = document.getElementById("outputText");
+document.addEventListener("DOMContentLoaded", function() {
+    const inputText = document.getElementById("inputText");
+    const convertButton = document.getElementById("convertButton");
+    const outputText = document.getElementById("outputText");
 
-// Add a click event listener to the convert button
-convertButton.addEventListener("click", () => {
-    // Get the text from the input textarea and convert it to uppercase
-    const text = inputText.value;
-    const uppercaseText = text.toUpperCase();
 
-    // Display the uppercase text in the output div
-    outputText.textContent = uppercaseText;
-    console.log(outputText);
+    convertButton.addEventListener("click", function() {
+        const text = inputText.value;
+
+        const convertedText = text.toUpperCase();
+
+        outputText.textContent = convertedText;
+    });
 });
