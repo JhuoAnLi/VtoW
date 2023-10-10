5d471dd459145c5cecff88249c5700150dc877bf
jalee <tommy920125@gmail.com>
2023-10-10 21:59:23 +0800

    commit

--- IME extension/popup.js
+++ IME extension/popup.js
@@ -1,32 +1,10 @@
-// // popup.js
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     const menuContainer = document.getElementById("menu-container");
-
-
-//     menuContainer.addEventListener("click", function(event) {
-//         if (event.target.classList.contains("menu-item")) {
-//             const selectedText = event.target.innerText;
-//             chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-//                 const activeTab = tabs[0];
-//                 chrome.scripting.executeScript({
-//                     target: { tabId: activeTab.id },
-//                     function: function(selectedText) {
-
-//                         const inputElement = document.activeElement;
-//                         if (inputElement) {
-//                             const startPosition = inputElement.selectionStart;
-//                             const endPosition = inputElement.selectionEnd;
-//                             const currentValue = inputElement.value;
-//                             const newValue = currentValue.substring(0, startPosition) +
-//                                 selectedText +
-//                                 currentValue.substring(endPosition);
-//                             inputElement.value = newValue;
-//                         }
-//                     },
-//                     args: [selectedText]
-//                 });
-//             });
-//         }
-//     });
-// });
+document.getElementById('toggleButton').addEventListener('click', function() {
+    var button = document.getElementById('toggleButton');
+    if (button.innerText === 'Multilingual IME unactivate') {
+        button.innerText = 'Multilingual IME activate';
+        button.classList.add('activate');
+    } else {
+        button.innerText = 'Multilingual IME unactivate';
+        button.classList.remove('activate');
+    }
+});
