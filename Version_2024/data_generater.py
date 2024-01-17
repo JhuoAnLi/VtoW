843b42920161ef9cf2cccb1c8489649e10aed9f0
JY <90738099+JY9150@users.noreply.github.com>
2024-01-17 16:58:59 +0800

    update data generater

--- Version_2024/data_generater.py
+++ Version_2024/data_generater.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     PLAIN_TEXT_DATASET_PATH = ".\\Dataset\\Plain_Text_Datasets\\"
     TARGET_DATASET_PATH = ".\\Dataset\\Key_Stroke_Datasets\\"
 
-    files = ["English.txt"]
-    convert_types = ["english"]#, "cangjie", "bopomofo", "pinyin"]
+    files = ["Chinese_cleaned.txt"]
+    convert_types = ["bopomofo"] #, "pinyin"]
     error_rates = [0, 0.001, 0.003, 0.01 ,0.05, 0.1]
 
     
@@ -17,19 +17,24 @@ if __name__ == '__main__':
         with open(PLAIN_TEXT_DATASET_PATH + file, "r", encoding="utf-8") as file:
             lines = file.readlines()
 
-                
-
             for convert_type in convert_types:
                 for error_rate in error_rates:
                     filename = "{}-{}.txt".format(convert_type, str(error_rate).replace(".", "_"))
 
                     if os.path.exists(TARGET_DATASET_PATH + filename) == False:
-                        with open(TARGET_DATASET_PATH + filename, "w", encoding="utf-8") as file2:
+                        try:
+                            data = ""
                             for line in lines:
-                                file2.write(TypoGenerater.generate(KeyStrokeConverter.convert(line.strip(), convert_type), error_rate) + "\n")
+                                data += TypoGenerater.generate(KeyStrokeConverter.convert(line.strip(), convert_type), error_rate) + "\n"
+                            
+                            translation_table = str.maketrans({str(i): "" for i in range(10)})
 
+                            
 
-    # input_string = "你好嗎"
-    # convert_type = "pinyin"
-    # print(KeyStrokeConverter.convert(input_string, convert_type))
-    # print(TypoGenerater.generate(input_string, 0.2))
+                            with open(TARGET_DATASET_PATH + filename, "w", encoding="utf-8") as file2:
+                                    file2.write(data)
+                            print("Dataset with keystroke type: {} error rate: {}, created successfully: ".format(convert_type, error_rate) + filename)
+                        except Exception as e:
+                            print("Error: " + str(e))
+                    else:
+                        print("File already exists: " + filename)
