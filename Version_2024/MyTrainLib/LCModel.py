a4e8f2e420ba8e5cb71da07fe173ade8f945494d
JY <90738099+JY9150@users.noreply.github.com>
2024-01-27 16:42:50 +0800

    Add LCModel class to MyTrainLib

---
+++ Version_2024/MyTrainLib/LCModel.py
@@ -0,0 +1,43 @@
+from transformers import AutoModel
+from torch import nn
+
+class LCModel(nn.Module): 
+    def __init__(
+            self,
+            layers=[],
+            device=None # fix this
+        ) -> None:
+        """
+        layers: list of layer sizes
+        device: device to run model on
+        """
+
+
+        super().__init__()
+
+        self.device = device
+        self.input_size = layers[0]
+        self.output_size = layers[-1]
+        self.num_layers = len(layers)
+
+        self.layer_list = nn.ModuleList()
+        for i in range(self.num_layers - 1):
+            self.layer_list.append(nn.Linear(layers[i], layers[i + 1]))
+            self.layer_list.append(nn.ReLU())
+        self.layer_list.append(nn.Sigmoid())
+
+
+    def forward(self, x):
+        for layer in self.layer_list:
+            x = layer(x)
+        return x
+
+
+
+    
+
+
+
+
+
+
