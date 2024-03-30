a86e728fe95b716d81b6ea2a12e12e5bc93f9791
jalee <tommy920125@gmail.com>
2024-03-30 21:39:15 +0800

    update SVM model

--- Version_2024/System/SVMClassification.py
+++ Version_2024/System/SVMClassification.py
@@ -9,12 +9,50 @@ import os
 import joblib
 from sklearn.metrics import confusion_matrix
 import numpy as np
+import re
 
 
 def read_file(filename):
     with open(filename, "r", encoding="utf-8") as f:
         lines = f.readlines()
     return [line.strip("\n").split("\t") for line in lines]
+
+
+def custom_tokenizer_bopomofo(text):
+    if not text:
+        return []
+    pattern = re.compile(r"(?<=3|4|6|7| )")
+    tokens = pattern.split(text)
+    tokens = [token for token in tokens if token]
+    if tokens[-1].find("§") != -1:
+        tokens.pop()
+
+    return tokens
+
+
+def custom_tokenizer_cangjie(text):
+    if not text:
+        return []
+    pattern = re.compile(r"(?<=[ ])")
+    tokens = pattern.split(text)
+    tokens = [token for token in tokens if token]
+    if tokens[-1].find("§") != -1:
+        tokens.pop()
+    return tokens
+
+
+def custom_tokenizer_pinyin(text):
+    if not text:
+        return []
+    tokens = []
+    pattern = re.compile(
+        r"(?:[bpmfdtnlgkhjqxzcsyw]|[zcs]h)?(?:[aeiouv]?ng|[aeiou](?![aeiou])|[aeiou]?[aeiou]?r|[aeiou]?[aeiou]?[aeiou])"
+    )
+    matches = re.findall(pattern, text)
+    tokens.extend(matches)
+    if tokens[-1].find("§") != -1:
+        tokens.pop()
+    return tokens
 
 
 class IMEClassifier:
@@ -29,12 +67,12 @@ class IMEClassifier:
             test_size=0.2,
             random_state=42,
         )
-        self.labels = self.y_train
-        self.X = self.vectorizer.fit_transform(self.X_train)
 
     def train_classifier(self):
 
         threads = []
+        self.labels = self.y_train
+        self.X = self.vectorizer.fit_transform(self.X_train)
         for label in set(self.labels):
             y_binary = [1 if l == label else 0 for l in self.labels]
             thread = threading.Thread(
@@ -88,24 +126,28 @@ class IMEClassifier:
     def load_model(self, modelname):
         load_path = os.path.join(os.getcwd(), "Version_2024\\Model_dump\\")
         self.classifiers = joblib.load(load_path + modelname)
-        # self.vectorizer = joblib.load(load_path + "vectorizer_" + modelname)
+        self.vectorizer = joblib.load(load_path + "vectorizer_" + modelname)
 
 
 if __name__ == "__main__":
 
-    filename = "Version_2024\\Train\\Dataset\\Train_Datasets\\pinyin-0-len20.txt"
+    filename = "Version_2024\\Train\\Dataset\\Train_Datasets\\english-0-len3.txt"
     data = read_file(filename)
     random.seed(42)
     training_random_data = random.sample(data, 300000)
-    # training_random_data_list = [(line[0], int(line[1])) for line in training_random_data]
+    training_random_data_list = [
+        (line[0], int(line[1])) for line in training_random_data
+    ]
 
-    test_file_name = "Version_2024\\Train\\Dataset\\Test_Datasets\\pinin-news-0-len5.txt"
-    temp_test_data = read_file(test_file_name)
-    testing_random_data = random.sample(temp_test_data, 10000)
-    testing_random_data_list = [(line[0], int(line[1])) for line in testing_random_data]
+    # test_file_name = (
+    #     "Version_2024\\Train\\Dataset\\Test_Datasets\\cangjie-0-len10-test.txt"
+    # )
+    # temp_test_data = read_file(test_file_name)
+    # testing_random_data = random.sample(temp_test_data, 100000)
+    # testing_random_data_list = [(line[0], int(line[1])) for line in testing_random_data]
 
     relative_path = "Version_2024\\Model_dump"
-    model_name = "model_pinyin-len20.pkl"
+    model_name = "english.pkl"
     if os.path.exists(os.path.join(os.getcwd(), relative_path, model_name)):
         loaded_classifier = IMEClassifier(training_random_data)
         loaded_classifier.load_model(model_name)
@@ -120,24 +162,34 @@ if __name__ == "__main__":
         print("Model saved")
         print("Model loaded")
 
-    # new_text = ["cl3", "jmam"]
-    # for text in new_text:
-    #     print("Test case:", text)
-    #     prediction = loaded_classifier.predict(text)
-    #     print("Predicted label:", prediction)
-
-    print("Length of testing data:", len(testing_random_data_list))
-    true_labels = []
-    predicted_labels = []
-
-    for text, true_label in testing_random_data_list:
+    new_text = ["cl3", "jmam"]
+    for text in new_text:
+        print("Test case:", text)
         prediction = loaded_classifier.predict(text)
-        true_labels.append(true_label)
-        predicted_labels.append(int(prediction))
+        print("Predicted label:", prediction)
 
-    conf_matrix = confusion_matrix(true_labels, predicted_labels)
-    print("Confusion Matrix:")
-    print(conf_matrix)
+    # print("Length of testing data:", len(testing_random_data_list))
+    # true_labels = []
+    # predicted_labels = []
 
-    accuracy = np.trace(conf_matrix) / np.sum(conf_matrix) * 100
-    print("Test Accuracy:", accuracy)
+    # with open("cangjie_len10_prediction_errors.txt", "w", encoding="utf-8") as f:
+    #     for text, true_label in testing_random_data_list:
+    #         prediction = loaded_classifier.predict(text)
+    #         true_labels.append(true_label)
+    #         predicted_labels.append(int(prediction))
+
+    #     for text, true_label, predicted_label in zip(
+    #         testing_random_data_list, true_labels, predicted_labels
+    #     ):
+    #         if true_label != predicted_label:
+    #             f.write("Text: {}\n".format(text))
+    #             f.write("True Label: {}\n".format(true_label))
+    #             f.write("Predicted Label: {}\n".format(predicted_label))
+    #             f.write("\n")
+
+    # conf_matrix = confusion_matrix(true_labels, predicted_labels)
+    # print("Confusion Matrix:")
+    # print(conf_matrix)
+
+    # accuracy = np.trace(conf_matrix) / np.sum(conf_matrix) * 100
+    # print("Test Accuracy:", accuracy)
