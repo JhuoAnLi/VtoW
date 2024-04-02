25c13744f0b3734af4c4352d613a6c9e89c83a5c
JY <90738099+JY9150@users.noreply.github.com>
2024-04-02 14:41:54 +0800

    update to meet new naming format

--- Version_2024/Train/Dataset/test_data_generater.py
+++ Version_2024/Train/Dataset/test_data_generater.py
@@ -8,49 +8,36 @@ if __name__ == "__main__":
     SRC_DATASET_PATH = ".\Test_Datasets\\"
     TARGET_DATASET_PATH = ".\Test_Datasets\\"
 
-    cut_sizes = [0]
     target_languages = ["bopomofo", "cangjie", "pinyin", "english"]
-    error_rates = [0, 0.1]
-    data_file_name_prefix = ["bopomofo-cc100", "cangjie-cc100", "pinyin-cc100", "english"]  # todo: fix here
+    prefixes = ["0", "r0-1", "r0-01"]
 
-    for file in os.listdir(SRC_DATASET_PATH):
-        if not file.endswith("-test.txt"):
-            os.remove(SRC_DATASET_PATH + file)
-        elif file.find("-len") > 0:
+
+    for file in os.listdir(TARGET_DATASET_PATH):
+        if not file.endswith("_test.txt"):
             os.remove(TARGET_DATASET_PATH + file)
-
+        elif file.find("labeled_") >= 0:
+            os.remove(TARGET_DATASET_PATH + file)
+    
     for target_language in target_languages:
-        for error_rate in error_rates:
-            for cut_size in cut_sizes:
-
-                CUT_SIZE = "x"
-                TARGET_LANGUAGE = target_language
-                ERROR_RATE = error_rate
-
-                target_file_name = "{}-{}-len{}-test.txt".format(TARGET_LANGUAGE, str(ERROR_RATE).replace(".", "_"), CUT_SIZE)
-
+        for prefix in prefixes:
+                files = os.listdir(SRC_DATASET_PATH)
+                src_file_names = [file for file in files if file.startswith(f"{prefix}_")]
+                assert len(src_file_names) == 4
+                target_file_name = f"labeled_{target_language}_{prefix}_test.txt"
                 if os.path.exists(TARGET_DATASET_PATH + target_file_name):
                     if "y" == input("File already exists. Do you want to overwrite it? (y/n)"):
                         print("Removing old file: " + target_file_name)
                         os.remove(TARGET_DATASET_PATH + target_file_name)
                     else:
-                        print("File not overwritten. Continue...")
-                        continue
+                        print("File not overwritten. Exiting...")
+                        exit()
 
-                error_rate_name = str(ERROR_RATE).replace(".", "_")
-
-
-                data_file_names = [f"{data_file_name_prefix[i]}-{error_rate_name}-test.txt" for i in range(4)]
-
-                for file_name in tqdm(data_file_names, desc="Processing files"):
+                for file_name in tqdm(src_file_names, desc="Processing files"):
                     with open(SRC_DATASET_PATH + file_name, "r", encoding="utf-8") as file:
                         lines = file.readlines()
-                        # lines = [line.replace("\n", "") for line in lines] # remove the \n in the line
-                        # joined_lines = "".join(lines)
-                        # new_lines = [joined_lines[i:i + CUT_SIZE] for i in range(0, len(joined_lines), CUT_SIZE)]
-                        new_lines = [line.replace("\n", "") for line in lines]
-                        new_lines = [line + "\t1" if TARGET_LANGUAGE in file_name else line + "\t0" for line in new_lines]
+                        lines = [line.replace("\n", "") for line in lines] # remove the \n in the line
+                        new_lines = [line + "\t1" if target_language in file_name else line + "\t0" for line in lines]
                     
-                    with open(TARGET_DATASET_PATH + target_file_name, "a", encoding="utf-8") as file:
+                    with open(os.path.join(TARGET_DATASET_PATH, target_file_name), "a", encoding="utf-8") as file:
                         for line in new_lines:
                             file.write(line + "\n")
