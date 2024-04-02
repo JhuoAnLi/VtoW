278f790cf98e0eee5c27f0d476230d15e6835bfe
JY <90738099+JY9150@users.noreply.github.com>
2024-04-02 13:37:33 +0800

    upadate to match new data naming format

--- Version_2024/Train/Dataset/train_data_generater.py
+++ Version_2024/Train/Dataset/train_data_generater.py
@@ -7,39 +7,36 @@ if __name__ == "__main__":
     TARGET_DATASET_PATH = ".\\Train_Datasets\\"
 
     target_languages = ["bopomofo", "cangjie", "pinyin", "english"]
-    error_rates = [0]
+    prefixes = ["0", "r0-1", "r0-01"]
 
-
-    data_file_names = ["bopomofo-cc100-0-train.txt", "cangjie-cc100-0-train.txt", "pinyin-cc100-0-train.txt", "english-0-train.txt"]
 
     for file in os.listdir(TARGET_DATASET_PATH):
-        if not file.endswith("-train.txt"):
+        if not file.endswith("_train.txt"):
             os.remove(TARGET_DATASET_PATH + file)
-        elif file.find("-len") > 0:
+        elif file.find("labeled_") >= 0:
             os.remove(TARGET_DATASET_PATH + file)
     
     for target_language in target_languages:
-        for error_rate in error_rates:
-            TARGET_LANGUAGE = target_language
-            ERROR_RATE = error_rate
-            CUT_SIZE = 3
+        for prefix in prefixes:
+                files = os.listdir(SRC_DATASET_PATH)
+                src_file_names = [file for file in files if file.startswith(f"{prefix}_")]
+                print(src_file_names)
+                assert len(src_file_names) == 4
+                target_file_name = f"labeled_{target_language}_{prefix}_train.txt"
+                if os.path.exists(TARGET_DATASET_PATH + target_file_name):
+                    if "y" == input("File already exists. Do you want to overwrite it? (y/n)"):
+                        print("Removing old file: " + target_file_name)
+                        os.remove(TARGET_DATASET_PATH + target_file_name)
+                    else:
+                        print("File not overwritten. Exiting...")
+                        exit()
 
-            target_file_name = "{}-{}-len{}.txt".format(TARGET_LANGUAGE, str(ERROR_RATE).replace(".", "_"), CUT_SIZE)
-
-            if os.path.exists(TARGET_DATASET_PATH + target_file_name):
-                if "y" == input("File already exists. Do you want to overwrite it? (y/n)"):
-                    print("Removing old file: " + target_file_name)
-                    os.remove(TARGET_DATASET_PATH + target_file_name)
-                else:
-                    print("File not overwritten. Exiting...")
-                    exit()
-
-            for file_name in tqdm(data_file_names, desc="Processing files"):
-                with open(SRC_DATASET_PATH + file_name, "r", encoding="utf-8") as file:
-                    lines = file.readlines()
-                    lines = [line.replace("\n", "") for line in lines] # remove the \n in the line
-                    new_lines = [line + "\t1" if TARGET_LANGUAGE in file_name else line + "\t0" for line in lines]
-                
-                with open(os.path.join(TARGET_DATASET_PATH, target_file_name), "a", encoding="utf-8") as file:
-                    for line in new_lines:
-                        file.write(line + "\n")
+                for file_name in tqdm(src_file_names, desc="Processing files"):
+                    with open(SRC_DATASET_PATH + file_name, "r", encoding="utf-8") as file:
+                        lines = file.readlines()
+                        lines = [line.replace("\n", "") for line in lines] # remove the \n in the line
+                        new_lines = [line + "\t1" if target_language in file_name else line + "\t0" for line in lines]
+                    
+                    with open(os.path.join(TARGET_DATASET_PATH, target_file_name), "a", encoding="utf-8") as file:
+                        for line in new_lines:
+                            file.write(line + "\n")
