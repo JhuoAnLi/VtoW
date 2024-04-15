5d34ab9129ac1e389fadd49854961142ee4d7a95
jalee <tommy920125@gmail.com>
2024-04-15 15:25:54 +0800

    update separator

--- Version_2024/System/IMESeparator.py
+++ Version_2024/System/IMESeparator.py
@@ -27,66 +27,97 @@ class IMESeparator:
             "..\\Model_dump\\vectorizer_pinyin_8adj.pkl",
         )
 
+    # def separate(self, input_keystrokes: str) -> list[list[str, str]]:
+    #     results = []
+    #     for seclen in range(3, 7):
+    #         while seclen < len(input_keystrokes):
+    #             former = input_keystrokes[:seclen]
+    #             latter = input_keystrokes[seclen:]
+    #             for method, detector in [
+    #                 ("bopomofo", self.my_bopomofo_detector),
+    #                 ("english", self.my_eng_detector),
+    #                 ("cangjie", self.my_cangjie_detector),
+    #                 ("pinyin", self.my_pinyin_detector),
+    #             ]:
+    #                 if (
+    #                     detector.predict(former) == False
+    #                     and detector.predict(latter) == True
+    #                 ):
+    #                     print(
+    #                         method + " " + former + " " + str(detector.predict(former)),
+    #                         latter + " " + str(detector.predict(latter)),
+    #                     )
+    #                     found = False
+    #                     for j, (existing_method, existing_value) in enumerate(results):
+    #                         if existing_method == method:
+    #                             results[j] = [
+    #                                 existing_method,
+    #                                 existing_value + " " + latter,
+    #                             ]
+    #                             found = True
+    #                             break
+    #                     if not found:
+    #                         results.append([method, latter])
+
+    #                 elif (
+    #                     detector.predict(former) == True
+    #                     and detector.predict(latter) == False
+    #                 ):
+    #                     print(
+    #                         method + " " + former + " " + str(detector.predict(former)),
+    #                         latter + " " + str(detector.predict(latter)),
+    #                     )
+    #                     found = False
+    #                     for j, (existing_method, existing_value) in enumerate(results):
+    #                         if existing_method == method:
+    #                             results[j] = [
+    #                                 existing_method,
+    #                                 existing_value + " " + former,
+    #                             ]
+    #                             found = True
+    #                             break
+    #                     if not found:
+    #                         results.append([method, former])
+    #                 elif (
+    #                     detector.predict(former) == False
+    #                     and detector.predict(latter) == False
+    #                 ):
+    #                     print(
+    #                         method + " " + former + " " + str(detector.predict(former)),
+    #                         latter + " " + str(detector.predict(latter)),
+    #                     )
+    #             seclen += seclen
+    #     return results
     def separate(self, input_keystrokes: str) -> list[list[str, str]]:
         results = []
-        for seclen in range(3, 4):
-            while seclen < len(input_keystrokes):
-                former = input_keystrokes[:seclen]
-                latter = input_keystrokes[seclen:]
-                for method, detector in [
-                    ("bopomofo", self.my_bopomofo_detector),
-                    ("english", self.my_eng_detector),
-                    ("cangjie", self.my_cangjie_detector),
-                    ("pinyin", self.my_pinyin_detector),
-                ]:
-                    if (
-                        detector.predict(former) == False
-                        and detector.predict(latter) == True
-                    ):
-                        print(
-                            method + " " + former + " " + str(detector.predict(former)),
-                            latter + " " + str(detector.predict(latter)),
-                        )
-                        found = False
-                        for j, (existing_method, existing_value) in enumerate(results):
-                            if existing_method == method:
-                                results[j] = [
-                                    existing_method,
-                                    existing_value + " " + latter,
-                                ]
-                                found = True
-                                break
-                        if not found:
-                            results.append([method, latter])
-
-                    elif (
-                        detector.predict(former) == True
-                        and detector.predict(latter) == False
-                    ):
-                        print(
-                            method + " " + former + " " + str(detector.predict(former)),
-                            latter + " " + str(detector.predict(latter)),
-                        )
-                        found = False
-                        for j, (existing_method, existing_value) in enumerate(results):
-                            if existing_method == method:
-                                results[j] = [
-                                    existing_method,
-                                    existing_value + " " + former,
-                                ]
-                                found = True
-                                break
-                        if not found:
-                            results.append([method, former])
-                    elif (
-                        detector.predict(former) == False
-                        and detector.predict(latter) == False
-                    ):
-                        print(
-                            method + " " + former + " " + str(detector.predict(former)),
-                            latter + " " + str(detector.predict(latter)),
-                        )
-                seclen += seclen
+        for seclen in range(3, len(input_keystrokes) + 1):
+            former = input_keystrokes[:seclen]
+            latter = input_keystrokes[seclen:]
+            for method, detector in [
+                ("bopomofo", self.my_bopomofo_detector),
+                ("english", self.my_eng_detector),
+                ("cangjie", self.my_cangjie_detector),
+                ("pinyin", self.my_pinyin_detector),
+            ]:
+                if (
+                    detector.predict(former) == True
+                    and detector.predict(latter) == False
+                ):
+                    print(
+                        method + " " + former + " " + str(detector.predict(former)),
+                        latter + " " + str(detector.predict(latter)),
+                    )
+                    former_method = method
+                    for method, detector in [
+                        ("bopomofo", self.my_bopomofo_detector),
+                        ("english", self.my_eng_detector),
+                        ("cangjie", self.my_cangjie_detector),
+                        ("pinyin", self.my_pinyin_detector),
+                    ]:
+                        if detector.predict(latter) == True:
+                            results.append([[former_method, former], [method, latter]])
+        if len(results) == 0:
+            results.append([[input_keystrokes]])
         return results
 
     def clean_results(results):
@@ -94,7 +125,9 @@ class IMESeparator:
         for sublist in results:
             counts = Counter(sublist[1].split())
 
-            common_words = set(word for word in counts if counts[word] > 1)
+            common_words = set(
+                word for word in counts if counts[word] > 1 or counts[word] == 1
+            )
 
             cleaned_words = []
             for word in sublist[1].split():
@@ -113,4 +146,4 @@ if __name__ == "__main__":
         input_text = input("Enter text: ")
         results = my_separator.separate(input_text)
         print(results)
-        print(IMESeparator.clean_results(results))
+        # print(IMESeparator.clean_results(results))
