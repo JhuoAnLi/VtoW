9192369e388eaf56792374d60d8eee5a28a1b4a5
JY <90738099+JY9150@users.noreply.github.com>
2024-05-01 19:20:08 +0800

    combind level 1 2 3

--- Version_2024/System/IMEHandler.py
+++ Version_2024/System/IMEHandler.py
@@ -1,5 +1,8 @@
 
 from typing import Union
+from sentence_transformers import SentenceTransformer
+from sentence_transformers.util import cos_sim
+import time
 
 from IMESeparator import IMESeparator
 from IMEConverter import IMEConverter
@@ -17,7 +20,15 @@ class IMEHandler():
         self._pinyin_converter = IMEConverter(".\\keystroke_mapping_dictionary\\pinyin_dict.json")
         self._english_converter = IMEConverter(".\\keystroke_mapping_dictionary\\english_dict_with_frequency.json")
         self._separator = IMESeparator()
-    
+        self._language_model = None
+        self._load_language_model("all-MiniLM-L6-v2")
+
+    def _load_language_model(self, model_name: str) -> None:
+        start_time = time.time()
+        print(f"Loading model...")
+        self._language_model = SentenceTransformer(model_name)
+        print("Model loaded successfully!", time.time() - start_time, "seconds")
+
     def get_candidate_words(self, keystroke: str, prev_context: str = "") -> list[dict[str, Union[int, list[dict[str, Union[str, int]]]]]]:
         separate_possibilities = self._separator.separate(keystroke)
         output = []
@@ -43,21 +54,40 @@ class IMEHandler():
                 else:
                     raise ValueError("Invalid method: " + method)
                 
-            
+            logical_sentence = [g for g in logical_sentence if len(g) > 0]
+            # logical_sentence.sort(key=lambda x: x[0]["distance"])  # bad
+            # print("logical_sentence", logical_sentence)
             sum_distance = sum([g[0]["distance"] for g in logical_sentence])
-
+            
             output.append({
                 "total_distance": sum_distance,
                 "sentence": logical_sentence
                 })
-    
+        
+        output = sorted(output, key=lambda x: x["total_distance"])
+        min_total_distance = output[0]["total_distance"]
+        output = [g for g in output if g["total_distance"] <= min_total_distance]
+
+        if len(prev_context) > 0:
+            encoded_context = self._language_model.encode(prev_context)
+
+            for possible_sentence_group in output:
+                for logic_token in possible_sentence_group["sentence"]:
+                    for possible_word in logic_token:
+                        possible_word["similarity_score"] = float(cos_sim(self._language_model.encode(possible_word["word"]), encoded_context).item())  # bad
+                    logic_token.sort(key=lambda x: x["similarity_score"], reverse=True)
         return output
 
 
 if __name__ == "__main__":
     my_IMEHandler = IMEHandler()
     while True:
-        user_keystroke = input("Input text: ")
-        print(my_IMEHandler.get_candidate_words(user_keystroke))
+        context = input("Input Context: ")
+        user_keystroke = input("Input Keystroke: ")
+        result = my_IMEHandler.get_candidate_words(user_keystroke, prev_context=context)
+        # print(result)
+        
+        with open("output.json", "w", encoding="utf-8") as f:
+            f.write(str(result))
         print("===")
 
